c29e67de7d7a11454a87d88955b7922860f0de31
dennis <dennis@dennis-B760M-DS3H-DDR4>
2025-12-20 22:36:27 +0100

    move to maven modules

---
+++ ui/src/main/java/mnemonics/MnemonicsApplication.java
@@ -0,0 +1,14 @@
+package mnemonics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnemonicsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MnemonicsApplication.class, args);
+	}
+
+}
